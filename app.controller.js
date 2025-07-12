9126bce34c3887d0a61b25d061b428e7b882fdab
Adel nabil <nabiladel052@gmail.com>
2025-07-12 16:39:07 +0300

    test 5

--- app.controller.js
+++ app.controller.js
@@ -4,50 +4,40 @@ import UserRouter from "./src/modules/users/user.controller.js";
 import { GlobalError } from "./src/utils/Error/index.js";
 import ProductRouter from "./src/modules/product/product.controller.js";
 const bootstrap = (app, express) => {
-  // cors origin middleware
-  // app.use(
-  //   cors({
-  //     origin: [
-  //       "http://localhost:3000",
-  //       "http://localhost:5173",
-  //       "https://in-home-eight.vercel.app",
-  //     ],
-  //     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
-  //     credentials: true,
-  //   })
-  // );
-  // const whitelist = [
-  //   "http://localhost:3000",
-  //   "http://localhost:5173",
-  //   "https://in-home-eight.vercel.app",
-  // ];
-  // app.use((req, res, next) => {
-  //   const origin = req.headers.origin;
-  //   if (!whitelist.includes(origin)) {
-  //     return next(new Error("Forbidden", { cause: 403 }));
-  //   }
-  //   res.setHeader("Access-Control-Allow-Origin", origin);
-  //   res.setHeader("Access-Control-Allow-Methods", "*");
-  //   res.setHeader("Access-Control-Allow-Headers", "*");
-  //   res.setHeader("Access-Control-private-network", true);
-  //   return next();
-  // });
-  const whitelist = ["http://localhost:3000","http://localhost:5173","https://in-home-eight.vercel.app"];
-  const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin ||  whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
+
+
+  const whitelist = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://in-home-eight.vercel.app",
+  ];
+  app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (!whitelist.includes(origin)) {
+      return next(new Error("Forbidden", { cause: 403 }));
     }
-  },
-  credentials: true,
-  privateNetworkAccess: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}
-  app.use(cors(corsOptions))
-  // app.options("*", cors(corsOptions));
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-private-network", true);
+    return next();
+  });
+
+//   const whitelist = ["http://localhost:3000","http://localhost:5173","https://in-home-eight.vercel.app"];
+//   const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin ||  whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   },
+//   credentials: true,
+//   privateNetworkAccess: true,
+//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"]
+// }
+//   app.use(cors(corsOptions))
   // json middleware
   app.use(express.json());
   // users
