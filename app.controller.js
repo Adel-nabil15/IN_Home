6b19f5a87924503d6e75e56108dd8a124ad58ef6
Adel nabil <nabiladel052@gmail.com>
2025-07-12 16:08:50 +0300

    test

--- app.controller.js
+++ app.controller.js
@@ -7,17 +7,33 @@ import { GlobalError } from "./src/utils/Error/index.js";
 import ProductRouter from "./src/modules/product/product.controller.js";
 const bootstrap = (app, express) => {
   // cors origin middleware
-  app.use(
-    cors({
-      origin: [
-        "http://localhost:3000",
-        "http://localhost:5173",
-        "https://in-home-eight.vercel.app",
-      ],
-      methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
-      credentials: true,
-    })
-  );
+  // app.use(
+  //   cors({
+  //     origin: [
+  //       "http://localhost:3000",
+  //       "http://localhost:5173",
+  //       "https://in-home-eight.vercel.app",
+  //     ],
+  //     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+  //     credentials: true,
+  //   })
+  // );
+  const whitelist = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://in-home-eight.vercel.app",
+  ];
+  app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (!whitelist.includes(origin)) {
+      return res.status(403).send("Forbidden");
+    }
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-private-network", true);
+    return next();
+  });
   // json middleware
   app.use(express.json());
   // users
