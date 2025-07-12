30c4f3547893676d32c8cfd3f4788aa0cb6ff823
Adel nabil <nabiladel052@gmail.com>
2025-07-12 16:41:21 +0300

    end test

--- app.controller.js
+++ app.controller.js
@@ -5,39 +5,23 @@ import { GlobalError } from "./src/utils/Error/index.js";
 import ProductRouter from "./src/modules/product/product.controller.js";
 const bootstrap = (app, express) => {
 
-
-  const whitelist = [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "https://in-home-eight.vercel.app",
-  ];
-  app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (!whitelist.includes(origin)) {
-      return next(new Error("Forbidden", { cause: 403 }));
+  // cors oprtions
+  const whitelist = ["http://localhost:3000","http://localhost:5173","https://in-home-eight.vercel.app"];
+  const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin ||  whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
     }
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.setHeader("Access-Control-private-network", true);
-    return next();
-  });
-
-//   const whitelist = ["http://localhost:3000","http://localhost:5173","https://in-home-eight.vercel.app"];
-//   const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin ||  whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   },
-//   credentials: true,
-//   privateNetworkAccess: true,
-//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"]
-// }
-//   app.use(cors(corsOptions))
+  },
+  credentials: true,
+  privateNetworkAccess: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}
+  // use cors middleware with options
+  app.use(cors(corsOptions))
   // json middleware
   app.use(express.json());
   // users
