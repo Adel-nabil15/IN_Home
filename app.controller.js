d97a88a63a50d5f88a7286bf04499ae58ecc8715
Adel nabil <nabiladel052@gmail.com>
2025-07-13 02:22:00 +0300

    ad front end whitelist

--- app.controller.js
+++ app.controller.js
@@ -4,24 +4,28 @@ import UserRouter from "./src/modules/users/user.controller.js";
 import { GlobalError } from "./src/utils/Error/index.js";
 import ProductRouter from "./src/modules/product/product.controller.js";
 const bootstrap = (app, express) => {
-
   // cors oprtions
-  const whitelist = ["http://localhost:3000","http://localhost:5173","https://in-home-eight.vercel.app"];
+  const whitelist = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://in-home-eight.vercel.app",
+    "https://instore-iota.vercel.app",
+  ];
   const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin ||  whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  credentials: true,
-  privateNetworkAccess: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    privateNetworkAccess: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  };
   // use cors middleware with options
-  app.use(cors(corsOptions))
+  app.use(cors(corsOptions));
   // json middleware
   app.use(express.json());
   // users
